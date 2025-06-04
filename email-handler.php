2f1612baa657e01ab3fa9477bb28697de3674dac
robsj001 <robsj001@gmail.com>
2025-06-04 14:20:55 +0100

    Update email-handler.php

--- email-handler.php
+++ email-handler.php
@@ -1,6 +1,6 @@
 <?php 
 $errors = '';
-$myemail = 'yourname@website.com';//<-----Put Your email address here.
+$myemail = 'info@aristideanorchestra.com';//<-----Put Your email address here.
 if(empty($_POST['name'])  || 
    empty($_POST['email']) || 
    empty($_POST['message']))
@@ -34,18 +34,3 @@ if( empty($errors))
 	header('Location: contact-form-thank-you.html');
 } 
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
-<html>
-<head>
-	<title>Contact form handler</title>
-</head>
-
-<body>
-<!-- This page is displayed only if there is some error -->
-<?php
-echo nl2br($errors);
-?>
-
-
-</body>
-</html>
